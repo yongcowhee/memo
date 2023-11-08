fd7711a367fb62ef8ebc7524e22bd46124ea265f
yongcowhee <yongcowhee@gmail.com>
2023-11-08 15:31:36 +0900

    Delete, Update 구현 및 MySQL DB 연결

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -43,5 +43,26 @@ public class MemoController {
 
         return responseList;
     }
+
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto){
+        // 해당 메모가 DB에 존재하는지 확인
+        if(memoList.containsKey(id)){
+            Memo memo = memoList.get(id);
+            memo.update(requestDto);
+            return memo.getId();
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
+
+    @DeleteMapping("/memos/{id}")
+    public void deleteMemo(@PathVariable Long id){
+        if(memoList.containsKey(id)) {
+            memoList.remove(id);
+        } else {
+            throw new IllegalArgumentException("선택한 메모는 존재하지 않습니다.");
+        }
+    }
 }
 
