8274e63d6e447f2259670fd5fed4676d009228ea
yongcowhee <yongcowhee@gmail.com>
2023-11-14 15:38:49 +0900

    Memo @Entity Annotaion 추가, Hibernate 설정 추가, JPA Auditing 적용

--- src/main/java/com/sparta/memo/entity/Memo.java
+++ src/main/java/com/sparta/memo/entity/Memo.java
@@ -1,10 +1,7 @@
 package com.sparta.memo.entity;
 
 import com.sparta.memo.dto.MemoRequestDto;
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,7 +12,8 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @NoArgsConstructor
-public class Memo {
+@Entity
+public class Memo extends Timestamped{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
